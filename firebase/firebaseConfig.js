3deae15540d983d68c2b9e2caa09d68f8a9acc78
Mohammad Idrees <139784998+MohIdrees-Mohammadi@users.noreply.github.com>
2024-07-27 18:37:34 +0500

    first commit

--- firebase/firebaseConfig.js
+++ firebase/firebaseConfig.js
@@ -6,14 +6,7 @@ import ReactNativeAsyncStorage from '@react-native-async-storage/async-storage';
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyB-v5NlI8d0U7fWPhuSfO-iwHzJbhZ9680",
-  authDomain: "viseion-based-attendence-sys.firebaseapp.com",
-  databaseURL: "https://viseion-based-attendence-sys-default-rtdb.firebaseio.com",
-  projectId: "viseion-based-attendence-sys",
-  storageBucket: "viseion-based-attendence-sys.appspot.com",
-  messagingSenderId: "481530340476",
-  appId: "1:481530340476:web:786becdbae008a444355f9",
-  measurementId: "G-PNTFL021BZ"
+  
   };
 
 // Initialize Firebase
